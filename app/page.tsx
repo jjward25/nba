4d6110e89d525cb02b82c78ac0e848f139d88a0c
jjward25 <45015765+jjward25@users.noreply.github.com>
2024-05-30 21:25:32 -0400

    summaryUpdate

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
   return ( 
     <main className="flex min-h-screen flex-col mx-auto items-center justify-start p-10 w-auto max-w-[1250px]">
       <h1 className="text-2xl mb-4 tracking-tighter font-semibold">
-        The Annual NBA <em className='bg-clip-text text-transparent bg-gradient-to-r from-amber-600 via-neutral-400 to-red-800'>Blow-it-Up </em> Article
+        The Annual NBA <em className='bg-clip-text text-transparent bg-gradient-to-r from-amber-600 via-neutral-400 to-red-800'>Blow-it-Up </em> Post
       </h1>
       <div className='mt-3 mb-6 h-auto w-full border-2 border-cyan-600'/>
-      <em className='mb-4 mx-4 max-w-[750px]'>{`It's the end of the year so we're looking at the biggest trades we think each team could make this offseason to compete for a title next year.`}</em>
+      <em className='mb-4 mx-4 max-w-[750px]'>{`It's the end of the year and 29 teams have work to do, so we're looking at the biggest trades we think each team could make this offseason.`}</em>
       <em className='mb-4 mx-4 text-neutral-600 max-w-[750px]'>{`Individual player cards show player percentile ranks for this past season. So "0.99" for Trae Young AST means he was in the 99th percentile for Total Assists this year (very good).`}</em>
       <div className='mt-3 mb-6 h-auto w-full border-2 border-cyan-600'/>
       <Atl/> 
